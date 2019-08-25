0686e3af3964fd2df2f0040775b3ffc4c575f286
Laik4 <qx.oco.xp@gmail.com>
2019-08-25 15:45:20 +0900

    change some functions' name

--- toyotama.py
+++ toyotama.py
@@ -8,7 +8,7 @@ import subprocess
 import re
 
 
-def extract_flag(s, head, tail=''):
+def extract_flag(s: str, head: str, tail: str = '') -> List[str]:
     try:
         comp = re.compile(rf'{head}.*?{tail}')
         return re.findall(comp, s)
@@ -17,13 +17,12 @@ def extract_flag(s, head, tail=''):
         comp = re.compile(patt.encode())
         return re.findall(comp, s)
 
-
-def random_string(n):
+def random_string(n: int) -> str:
     return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(n)])
 
 
-def lcm(x, y):
-    return (x * y) // gcd(x, y)
+def lcm(x: int, y: int) -> int:
+    return x * y // gcd(x, y)
 
 
 def extended_gcd(a: int, b: int) -> Tuple[int, int, int]:
@@ -39,22 +38,23 @@ def extended_gcd(a: int, b: int) -> Tuple[int, int, int]:
     return a0, b0, c0
 
 
-def inv(a: int, n: int) -> int:
+
+def mod_inverse(a: int, n: int) -> int:
     s, _, _ = extgcd(a, n)
     return s % n
 
 
 def int_to_string(x: int, byte=False) -> str:
-    sb = x.to_bytes((x.bit_length()+7)//8, byteorder='big')
+    sb = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
     if byte:
         return sb
     else:
         return sb.decode()
 
 
+
 def string_to_int(s: str) -> int:
-    sn = int.from_bytes(s.encode(), 'big')
-    return sn
+    return int.from_bytes(s.encode(), 'big')
 
 
 def rot(n: int, s: str) -> str:
@@ -73,14 +73,13 @@ def rot(n: int, s: str) -> str:
 
 def xor_string(s: str, t: str) -> str:
     if isinstance(s, str):
-        return "".join(chr(ord(a) ^ ord(b)) for a, b in zip(s, t))
+        return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s, t))
     else:
         return bytes([a ^ b for a, b in zip(s, t)])
 
 
 def get_secretkey(e: int, p: int, q: int) -> int:
-    d = inv(e, (p - 1) * (q - 1))
-    return d
+    return inv(e, (p - 1) * (q - 1))
 
 
 def get_param(filename: str) -> Tuple[int]:
@@ -103,13 +102,9 @@ def chinese_remainder(a: List[int], p: List[int]) -> int:
     x = sum([ai * inv(P // pi, pi) * P // pi for ai, pi in zip(a, p)])
     return x % P
 
-
-
-
-
 # Crypto
 
-# Vigenere
+## Vigenere
 def vigenere(cipher: str, key: str) -> str:
     key = key.lower()
     key = [ord('a') - ord(c) for c in key]
@@ -125,7 +120,7 @@ def vigenere(cipher: str, key: str) -> str:
     return ''.join(ans)
 
 
-# Common Modulus Attack
+## Common Modulus Attack
 def common_modulus_attack(e1: int, e2: int, c1: int, c2: int, n: int) -> int:
     s1, s2, _ = extgcd(e1, e2)
     if s1 < 0:
@@ -138,7 +133,8 @@ def common_modulus_attack(e1: int, e2: int, c1: int, c2: int, n: int) -> int:
     return (pow(c1, s1, n) * pow(c2, s2, n)) % n
 
 
-### Wiener's Attack ###
+## Wiener's Attack
+
 def rat_to_cfrac(a: int, b: int) -> Iterator[int]:
     while b > 0:
         x = a // b
@@ -176,9 +172,11 @@ def wieners_attack(e: int, n: int) -> Optional[int]:
             return dg // g
     return None
 
-# Discrete Logarithm Problem
-# find x s.t. pow(g, x, p) == y
+## Discrete Logarithm Problem
+## find x s.t. pow(g, x, p) == y
 
+
+### Baby-step Giant-step algorithm
 def baby_giant(g: int, y: int, p: int) -> int:
     m = ceil(isqrt(p))
     table = {}
@@ -199,7 +197,7 @@ def baby_giant(g: int, y: int, p: int) -> int:
 
     return -1
 
-
+## Pohlig-Hellman algorithm
 def pohlig_hellman(g: int, y: int, p: int, phi_p: List[int]) -> int:
     print(phi_p)
     X = [baby_giant(pow(g, (p - 1) // q, p), pow(y, (p - 1) // q, p), q)
