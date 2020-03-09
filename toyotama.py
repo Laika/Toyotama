0a3e2326077193675dd39a3e99a3999f7bf54540
Laik4 <qx.oco.xp@gmail.com>
2020-03-10 04:34:09 +0900

    Add binary_to_image

--- toyotama.py
+++ toyotama.py
@@ -484,6 +484,36 @@ def urldecode(s, encoding='shift-jis'):
     from urllib.parse import unquote_plus
     return unquote_plus(s, encoding=encoding)
 
+def binary_to_image(data, padding=5, size=5, rev=False, image_size=(1000, 1000)):
+    from PIL import Image, ImageDraw
+    bk, wh = (0, 0, 0), (255, 255, 255)
+    image = Image.new('RGB', image_size, wh)
+    rect = Image.new('RGB', (size, size))
+    draw = ImageDraw.Draw(rect)
+    draw.rectangle((0, 0, size, size), fill=bk)
+
+    h, w = 0, 0
+    x, y = 0, 0
+    for pixel in data:
+        if pixel == '\n':
+            y += 1
+            h += 1
+            w = max(w, x)
+            x = 0
+        else:
+            if (pixel == '1') ^ rev:
+                image.paste(rect, (padding+x*size, padding+y*size))
+            x += 1
+
+    return image.crop((0, 0, 2*padding+w*size, 2*padding+h*size))
+
+
+
+
+
+
+
+
 
     
 # pwn
