00e2572c5375828350410d6b9d333396ad77c3b8
Laik4 <qx.oco.xp@gmail.com>
2020-03-18 10:31:39 +0900

    Fix fill

--- toyotama.py
+++ toyotama.py
@@ -481,7 +481,7 @@ u8   = lambda x, sign=False: unpack('<B' if not sign else '<b', x)[0]
 u16  = lambda x, sign=False: unpack('<H' if not sign else '<h', x)[0] 
 u32  = lambda x, sign=False: unpack('<I' if not sign else '<i', x)[0] 
 u64  = lambda x, sign=False: unpack('<Q' if not sign else '<q', x)[0] 
-fill = lambda x, c='A', byte=True: bytes([b'A' for _ in range(x)]) if byte else c*x
+fill = lambda x, c='A', byte=True: (c*x).encode() if byte else c*x
 
 
 # crypto
