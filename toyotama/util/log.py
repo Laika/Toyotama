49be49d25cf7d827328a01e369ba67d56fc4c444
Laika <laika@albina.cc>
2021-05-16 14:20:00 +0900

    Update Style

--- toyotama/util/log.py
+++ toyotama/util/log.py
@@ -1,37 +1,34 @@
 import sys
-from collections import namedtuple
+from enum import Enum
 
-Style = namedtuple(
-    "Style",
-    "RESET BOLD RED GREEN YELLOW BLUE MAGENTA CYAN PURPLE VIOLET DEEP_PURPLE ORANGE DARK_GREY GREY",
-)(
-    RESET="\x1b[0m",
-    BOLD="\x1b[1m",
-    FG_RED="\x1b[38;5;{0xdc};{0x35};{0x45}m",
-    BG_RED="\x1b[48;5;{0xdc};{0x35};{0x45}m",
-    GREEN="\x1b[38;5;2m",
-    FG_YELLOW=f"\x1b[38;5;{0xff};{0xc1};{0x07}m",
-    BG_YELLOW=f"\x1b[48;5;{0xff};{0xc1};{0x07}m",
-    FG_BLUE="\x1b[38;5;4m",
-    BG_BLUE="\x1b[48;5;4m",
-    FG_MAGENTA="\x1b[38;5;5m",
-    BG_MAGENTA="\x1b[48;5;5m",
-    CYAN="\x1b[38;5;6m",
-    PURPLE="\x1b[38;5;93m",
-    VIOLET="\x1b[38;5;128m",
-    FG_VIOLET=f"\x1b[38;5;{0x80};{0x00};{0x80}m",
-    BG_VIOLET=f"\x1b[48;5;{0x80};{0x00};{0x80}m",
-    FG_DEEPPURPLE=f"\x1b[38;5;{0x70};{0x00};{0x70}m",
-    BG_DEEPPURPLE=f"\x1b[48;5;{0x70};{0x00};{0x70}m",
-    FG_ORANGE=f"\x1b[38;5;{0xff};{0xc1};{0x07}m",
-    BG_ORANGE=f"\x1b[48;5;{0xff};{0xc1};{0x07}m",
-    DARK_GREY="\x1b[38;5;240m",
-    GREY="\x1b[38;5;246m",
-    FG_WHITE=f"\x1b[38;5;{0xff};{0xff};{0xff}m",
-    BG_WHITE=f"\x1b[48;5;{0xff};{0xff};{0xff}m",
-    FG_BLACK=f"\x1b[38;5;{0x00};{0x00};{0x00}m",
-    BG_BLACK=f"\x1b[48;5;{0x00};{0x00};{0x00}m",
-)
+
+class Style(Enum):
+    RESET = "\x1b[0m"
+    BOLD = "\x1b[1m"
+    FG_RED = f"\x1b[38;5;{0xdc};{0x35};{0x45}m"
+    BG_RED = f"\x1b[48;5;{0xdc};{0x35};{0x45}m"
+    GREEN = "\x1b[38;5;2m"
+    FG_YELLOW = f"\x1b[38;5;{0xff};{0xc1};{0x07}m"
+    BG_YELLOW = f"\x1b[48;5;{0xff};{0xc1};{0x07}m"
+    FG_BLUE = "\x1b[38;5;4m"
+    BG_BLUE = "\x1b[48;5;4m"
+    FG_MAGENTA = "\x1b[38;5;5m"
+    BG_MAGENTA = "\x1b[48;5;5m"
+    CYAN = "\x1b[38;5;6m"
+    PURPLE = "\x1b[38;5;93m"
+    VIOLET = "\x1b[38;5;128m"
+    FG_VIOLET = f"\x1b[38;5;{0x80};{0x00};{0x80}m"
+    BG_VIOLET = f"\x1b[48;5;{0x80};{0x00};{0x80}m"
+    FG_DEEPPURPLE = f"\x1b[38;5;{0x70};{0x00};{0x70}m"
+    BG_DEEPPURPLE = f"\x1b[48;5;{0x70};{0x00};{0x70}m"
+    FG_ORANGE = f"\x1b[38;5;{0xff};{0xc1};{0x07}m"
+    BG_ORANGE = f"\x1b[48;5;{0xff};{0xc1};{0x07}m"
+    DARK_GREY = "\x1b[38;5;240m"
+    GREY = "\x1b[38;5;246m"
+    FG_WHITE = f"\x1b[38;5;{0xff};{0xff};{0xff}m"
+    BG_WHITE = f"\x1b[48;5;{0xff};{0xff};{0xff}m"
+    FG_BLACK = f"\x1b[38;5;{0x00};{0x00};{0x00}m"
+    BG_BLACK = f"\x1b[48;5;{0x00};{0x00};{0x00}m"
 
 
 class Logger:
